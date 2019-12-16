113ab682c15c65c152a0cf3f6f69c81794ab5784
Muhammad Gaffar <gafar98@gmail.com>
2019-12-16 16:33:31 +0700

    add kmesh test and test badge

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# PythTB
+# PythTB.jl
+
+![Build Status](https://circleci.com/gh/muhammadgaffar/PythTB.jl/)
+
+This is Julia Wrapper for PythTB Package.
--- test/pythtb.jl
+++ test/pythtb.jl
@@ -23,6 +23,16 @@ function test_model_nspin2()
     return model
 end
 
+function test_model_kmesh()
+    lat = [0.8 0.2 0; 0.5 3/2 1; 0 0 2]
+    orb = [0 0 0; 1 1 1]
+    model = tb_model(3,3,lat,orb)
+    set_hop!(model,1,1,1,[1,0,0])
+    set_hop!(model,1,1,1,[0,1,1])
+    set_hop!(model,0.8,1,2,[0,0,0])
+    return model
+end
+
 #test functionality on generating model with spin = 1
 @testset "nspin1" begin
     model = test_model_nspin1()
@@ -40,4 +50,17 @@ end
     @test size(model.hoppings[1]) == (2,2)
     @test size(hamiltonian(model,[rand(),rand()])) == (2,2,2,2)
     @test size(val_to_block(model,2*σ.x)) == (2,2)
+end
+
+@testset "kmesh and pauli" begin
+    model = test_model_kmesh()
+    nk = 5
+    @test size(k_uniform_mesh(model,[nk,nk,nk])) == (nk^3,3)
+    path = [1 2 3; 2/3 3 1/2; 0 0 0]
+    nk = 24
+    k_vec,k_dist,k_node = k_path(model,path,nk)
+    @test size(k_vec) == (nk,model.dim_k)
+    @test length(k_dist) == nk
+    @test length(k_node) == size(path,2)
+    @test typeof(σ) == PythTB.pauli
 end
